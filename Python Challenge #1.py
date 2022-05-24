ee563ac0ec0082fb1c8cf88a201b1c26c647c5c9
Shadowy2003 <105732970+Shadowy2003@users.noreply.github.com>
2022-05-24 18:47:55 +0100

    Add files via upload

---
+++ Python Challenge #1.py
@@ -0,0 +1,6 @@
+#Python Challenge 1
+
+users = ["Anderson", "Ifeoma", "Olakunle", "Lee", "Margaret", "Olakunle", "Musa", "Musa", "Lee", "Anderson", "Yvonne", "Albert", "Ofori", "Ofori", "Rofiyat", "Rofiyat", "Dele", "Muhammed", "Rofiyat", "Ofori", "Lee", "Ifeoma"]
+
+print(len(users))
+file = open("users.txt", "w")
