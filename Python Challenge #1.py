754af77890f8a303b0b1b3d27208979bcd9bbcea
Shadowy2003 <105732970+Shadowy2003@users.noreply.github.com>
2022-05-24 18:49:28 +0100

    Delete Python Challenge #1.py

--- Python Challenge #1.py
+++
@@ -1,6 +0,0 @@
-#Python Challenge 1
-
-users = ["Anderson", "Ifeoma", "Olakunle", "Lee", "Margaret", "Olakunle", "Musa", "Musa", "Lee", "Anderson", "Yvonne", "Albert", "Ofori", "Ofori", "Rofiyat", "Rofiyat", "Dele", "Muhammed", "Rofiyat", "Ofori", "Lee", "Ifeoma"]
-
-print(len(users))
-file = open("users.txt", "w")
